f58441e1471ad02d7c2bbabdf5dde0c944a1b15e
jyw2671 <yjung@student.42seoul.kr>
2022-05-11 17:56:39 +0900

    add templete <is_integral>

--- includes/utils.hpp
+++ includes/utils.hpp
@@ -6,6 +6,7 @@
 namespace ft{
 	/*
 	 * enable_if
+	The type T is enabled as member type enable_if::type if Cond is true.
 	*/
 	template <bool Cond, typename T = void>
 	struct enable_if {};
@@ -17,6 +18,12 @@ namespace ft{
 
 	/*
 	 * is_integral
+	Checks whether T is an integral type.
+	bool, char, char16_t, char32_t, wchar_t, short, int, long, long long,
+	unsinged char, unsigned short, unsigned int, unsigned long, unsigned long long,
+	or any inplementation-defined extended integer types,
+	including any signed, unsigned, and cv-qualified variants.
+	Otherwise, value is equal to false.
 	*/
 	template <bool is_integral, typename T>
 	struct integral_traits {
@@ -28,6 +35,47 @@ namespace ft{
 	//char16_t, char32_t, long long, unsigned long long -> c++11
 	template <typename>
 	struct is_integral_type : public integral_traits<false, bool> {};
+
+	// template 특수화
+	// bool 타입에 대해 특수화
+	template <>
+	struct is_integral_type<bool> : public integral_traits<true, bool> {};
+
+	template <>
+	struct is_integral_type<char> : public integral_traits<true, bool> {};
+
+	template <>
+	struct is_integral_type<signed char> : public integral_traits<true, bool> {};
+
+	template <>
+	struct is_integral_type<wchar_t> : public integral_traits<true, bool> {};
+
+	template <>
+	struct is_integral_type<short int> : public integral_traits<true, bool> {};
+
+	template <>
+	struct is_integral_type<int> : public integral_traits<true, bool> {};
+
+	template <>
+	struct is_integral_type<long int> : public integral_traits<true, bool> {};
+
+	template <>
+	struct is_integral_type<long long int> : public integral_traits<true, bool> {};
+
+	template <>
+	struct is_integral_type<unsigned char> : public integral_traits<true, bool> {};
+
+	template <>
+	struct is_integral_type<unsigned short> : public integral_traits<true, bool> {};
+
+	template <>
+	struct is_integral_type<unsigned int> : public integral_traits<true, bool> {};
+
+	template <>
+	struct is_integral_type<unsigned long int> : public integral_traits<true, bool> {};
+
+	template <>
+	struct is_integral_type<unsigned long long int> : public integral_traits<true, bool> {};
 }	// namespace ft
 
 
