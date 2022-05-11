e980777f4259f5842746dea76d7b9e134add82fe
jyw2671 <yjung@student.42seoul.kr>
2022-05-11 17:37:55 +0900

    add is_integral <type_traits>

--- includes/utils.hpp
+++ includes/utils.hpp
@@ -14,6 +14,20 @@ namespace ft{
 	struct enable_if<true, T> {
 		typedef T type;
 	};
+
+	/*
+	 * is_integral
+	*/
+	template <bool is_integral, typename T>
+	struct integral_traits {
+		typedef T type;
+		static const bool value = is_integral;
+	};
+
+	//float, double, long double은 false로 처리된다.
+	//char16_t, char32_t, long long, unsigned long long -> c++11
+	template <typename>
+	struct is_integral_type : public integral_traits<false, bool> {};
 }	// namespace ft
 
 
