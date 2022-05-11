fa34a84be0affd52ce352cf29ef4f5d98bf0a16d
jyw2671 <yjung@student.42seoul.kr>
2022-05-11 17:35:22 +0900

    add enable_if <type_traits>

--- includes/utils.hpp
+++ includes/utils.hpp
@@ -4,6 +4,16 @@
 #include "iterator.hpp"
 
 namespace ft{
+	/*
+	 * enable_if
+	*/
+	template <bool Cond, typename T = void>
+	struct enable_if {};
+
+	template <typename T>
+	struct enable_if<true, T> {
+		typedef T type;
+	};
 }	// namespace ft
 
 
