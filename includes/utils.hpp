6fbb22052e399ecb89de054f017a3aa9ec0b11a1
jyw2671 <yjung@student.42seoul.kr>
2022-05-19 15:49:25 +0900

    add util-std::relational operators (pair)

--- includes/utils.hpp
+++ includes/utils.hpp
@@ -142,6 +142,41 @@ namespace ft{
 			}
 	};
 
+	/* std::relational operators (pair)
+		Performs the appropriate comparison operation between
+		the pair objects lhs and rhs.
+	*/
+	template <class T1, class T2>
+	bool operator== (const pair<T1,T2>& lhs, const pair<T1,T2>& rhs) {
+		return lhs.first==rhs.first && lhs.second==rhs.second;
+	}
+
+	template <class T1, class T2>
+	bool operator!= (const pair<T1,T2>& lhs, const pair<T1,T2>& rhs) {
+		return !(lhs==rhs);
+	}
+
+	template <class T1, class T2>
+	bool operator<  (const pair<T1,T2>& lhs, const pair<T1,T2>& rhs) {
+		return lhs.first<rhs.first ||
+			(!(rhs.first<lhs.first) && lhs.second<rhs.second);
+	}
+
+	template <class T1, class T2>
+	bool operator<= (const pair<T1,T2>& lhs, const pair<T1,T2>& rhs) {
+		return !(rhs<lhs);
+	}
+
+	template <class T1, class T2>
+	bool operator>  (const pair<T1,T2>& lhs, const pair<T1,T2>& rhs) {
+		return rhs<lhs;
+	}
+
+	template <class T1, class T2>
+	bool operator>= (const pair<T1,T2>& lhs, const pair<T1,T2>& rhs) {
+		return !(lhs<rhs);
+	}
+
 	// std::make_pair
 	template <class T1,class T2>
 	pair<T1,T2> make_pair (T1 x, T2 y) {
