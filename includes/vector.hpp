9ce918176c57bad9d78f31886c00438f0eb93212
jyw2671 <yjung@student.42seoul.kr>
2022-05-25 20:14:06 +0900

    add vector member function Capacity

--- includes/vector.hpp
+++ includes/vector.hpp
@@ -97,6 +97,43 @@ namespace ft {
 
 			size_type size() const { return (this->_end - this->_begin); }
 			size_type max_size() const { return (this->_alloc.max_size()); }
+
+			// n개의 val값들을 갖는 vector로 resize한다.
+			// n이 현재 컨테이너 크기보다 작으면, 그 이상의 요소를 제거하고 크기를 축소시킨다.
+			// n이 현재 컨테이너 크기보다 크면, n의 크기만큼 새로운 값을 삽입한다.
+			// value가 지정되면 새 요소가 value의 복사본으로 초기화되고, 그렇지 않으면 값이 초기화된다.
+			void resize(size_type n, value_type val = value_type()) {
+				if (n < size())
+					erase(_start + n, _end);
+				else if (n > size())
+					insert(_end, n - size(), val);
+			}
+
+			// element의 갯수가 아닌, 할당받은 메모리의 갯수. (잠재적 크기)
+			size_type capacity() const { return (this->_end_of_capacity - this->_start); }
+			bool empty() const { return (this->_start == this->_end); }
+
+			// 벡터의 용량(재할당 없이 벡터가 보유할 수 있는 총 요소 수)을 보다 크거나 같은 값으로 늘 new_cap립니다.
+			// 현재 capacity() new_cap 보다 크면 새 스토리지가 할당되고, 그렇지 않으면 함수가 아무 작업도 수행하지 않습니다.
+			void reserve(size_type n) {
+				if (n > max_size()) //최대 크기를 넘어가면 에러
+					throw(std::length_error("Error: ft::vector::reserve"));
+				else if (n > this->capacity()) {
+					pointer prev_start = this->_start;
+					pointer prev_end = this->_end;
+					pointer prev_end_of_capacity = this->_end_of_capacity;
+
+					this->_start = this->_alloc.allocate(n);
+					this->_end = this->_start;
+					this->_end_of_capacity = this->_start + n;
+					pointer tmp = prev_start;
+					while (tmp != prev_end) {
+						this->_alloc.construct(this->_end++, *tmp);
+						this->_alloc.destroy(tmp++);
+					}
+					this->_alloc.deallocate(prev_start, prev_end_of_capacity - prev_start);
+				}
+			}
 	}; // class vector
 }  // namespace ft
 
