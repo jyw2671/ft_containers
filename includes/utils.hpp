19ba01fc9f187dcb616400340643b0352558fdb8
jyw2671 <yjung@student.42seoul.kr>
2022-06-08 03:53:30 +0900

    add make_pair, binary function, less

--- includes/utils.hpp
+++ includes/utils.hpp
@@ -4,10 +4,7 @@
 #include "iterator.hpp"
 
 namespace ft{
-	/*
-	 * enable_if
-	The type T is enabled as member type enable_if::type if Cond is true.
-	*/
+	// The type T is enabled as member type enable_if::type if Cond is true.
 	template <bool Cond, typename T = void>
 	struct enable_if {};
 
@@ -18,12 +15,12 @@ namespace ft{
 
 	/*
 	 * is_integral
-	Checks whether T is an integral type.
-	bool, char, char16_t, char32_t, wchar_t, short, int, long, long long,
-	unsinged char, unsigned short, unsigned int, unsigned long, unsigned long long,
-	or any inplementation-defined extended integer types,
-	including any signed, unsigned, and cv-qualified variants.
-	Otherwise, value is equal to false.
+		Checks whether T is an integral type.
+		bool, char, char16_t, char32_t, wchar_t, short, int, long, long long,
+		unsinged char, unsigned short, unsigned int, unsigned long, unsigned long long,
+		or any inplementation-defined extended integer types,
+		including any signed, unsigned, and cv-qualified variants.
+		Otherwise, value is equal to false.
 	*/
 	template <bool is_integral, typename T>
 	struct integral_traits {
@@ -77,7 +74,11 @@ namespace ft{
 	template <>
 	struct is_integral_type<unsigned long long int> : public integral_traits<true, bool> {};
 
-	// equal
+	// T가 integral type인지 식별하는 class.
+	// is_integral_type< T > 를 상속받아 T에 따라 type을 다르게 갖는다.
+	template <typename T>
+	struct is_integral : public is_integral_type<T> {};	// equal
+
 	/* first1, last1
 		첫 번째 시퀀스의 초기 및 최종 위치에 반복자를 입력합니다 .
 		사용된 범위는 first1 이 가리키는 요소를 포함 하지만 last1 이 가리키는 요소가 아닌
@@ -116,9 +117,41 @@ namespace ft{
 		return true;
 	}
 
-	/* std:pair
-		Constructs a pair object.
-	*/
+	// lexicographical compare
+	// default
+	template <class InputIterator1, class InputIterator2>
+	bool lexicographical_compare (InputIterator1 first1, InputIterator1 last1,
+								InputIterator2 first2, InputIterator2 last2) {
+		while (first1 != last1) {
+			if (first2 == last2 || *first2 < *first1)
+				return (false);
+			else if (*first1 < *first2)
+				return (true);
+			++first1;
+			++first2;
+		}
+		return (first2 != last2);
+	};
+
+	// custom
+	// binary comparison function comp
+	// comparison function object (i.e. an object that satisfies the requirements of Compare)
+	// which returns ​true if the first argument is less than the second.
+	template <class InputIterator1, class InputIterator2, class Compare>
+	bool lexicographical_compare (InputIterator1 first1, InputIterator1 last1,
+								InputIterator2 first2, InputIterator2 last2, Compare comp) {
+		while (first1 != last1) {
+			if (first2 == last2 || comp(*first2, *first1))
+				return (false);
+			else if (cmop(*first1, *first2))
+				return (true);
+			++first1;
+			++first2;
+		}
+		return (first2 != last2);
+	};
+
+	// 두 객체를 하나의 객체로 취급 할 수 있게 묶어주는 클래스
 	template <class T1, class T2>
 	struct pair {
 		public:
@@ -128,60 +161,75 @@ namespace ft{
 			first_type first;
 			second_type second;
 
-			pair() : first(), second() {} // default
-
-			template<class U, class V>
-			pair(const pair<U,V>& pr) : first(pr.first), second(pr.second) {} // copy
-			pair(const first_type& a, const second_type& b) : first(a), second(b) {} // initialization
-			pair& operator= (const pair& pr) { // assign
+			//default
+			pair() : first(), second() {}
+			//copy
+			template<class U, class V> pair (const pair<U, V>& pr) : first(pr.first), second(pr.second) {}
+			//initialization
+			pair(const first_type& a, const second_type& b) : first(a), second(b) {}
+			//assign
+			pair& operator= (const pair& pr) {
 				if (this != &pr) {
 					this->first = pr.first;
 					this->second = pr.second;
 				}
-				return *this;
+				return (*this);
 			}
+	}; // class pair
+
+	// 변수1(x), 변수2(y)가 들어간 pair를 만들어준다
+	template <class T1, class T2>
+	pair<T1, T2> make_pair(T1 x, T2 y) {
+		return (pair<T1, T2>(x,y));
 	};
 
-	/* std::relational operators (pair)
-		Performs the appropriate comparison operation between
-		the pair objects lhs and rhs.
-	*/
 	template <class T1, class T2>
-	bool operator== (const pair<T1,T2>& lhs, const pair<T1,T2>& rhs) {
-		return lhs.first==rhs.first && lhs.second==rhs.second;
-	}
+	bool operator==(const pair<T1, T2> &lhs, const pair<T1, T2> &rhs) {
+		return (lhs.first == rhs.first && lhs.second == rhs.second);
+	};
 
 	template <class T1, class T2>
-	bool operator!= (const pair<T1,T2>& lhs, const pair<T1,T2>& rhs) {
-		return !(lhs==rhs);
-	}
+	bool operator!=(const pair<T1, T2> &lhs, const pair<T1, T2> &rhs) {
+		return (!(lhs == rhs));
+	};
 
 	template <class T1, class T2>
-	bool operator<  (const pair<T1,T2>& lhs, const pair<T1,T2>& rhs) {
-		return lhs.first<rhs.first ||
-			(!(rhs.first<lhs.first) && lhs.second<rhs.second);
-	}
+	bool operator<(const pair<T1, T2> &lhs, const pair<T1, T2> &rhs) {
+		return (lhs.first < rhs.first || (!(rhs.first < lhs.first) && lhs.second < rhs.second));
+	};
 
 	template <class T1, class T2>
-	bool operator<= (const pair<T1,T2>& lhs, const pair<T1,T2>& rhs) {
-		return !(rhs<lhs);
-	}
+	bool operator<=(const pair<T1, T2> &lhs, const pair<T1, T2> &rhs) {
+		return (!(rhs < lhs));
+	};
 
 	template <class T1, class T2>
-	bool operator>  (const pair<T1,T2>& lhs, const pair<T1,T2>& rhs) {
-		return rhs<lhs;
-	}
+	bool operator>(const pair<T1, T2> &lhs, const pair<T1, T2> &rhs) {
+		return (rhs < lhs);
+	};
 
 	template <class T1, class T2>
-	bool operator>= (const pair<T1,T2>& lhs, const pair<T1,T2>& rhs) {
-		return !(lhs<rhs);
-	}
+	bool operator>=(const pair<T1, T2> &lhs, const pair<T1, T2> &rhs) {
+		return !(lhs < rhs);
+	};
 
-	// std::make_pair
-	template <class T1,class T2>
-	pair<T1,T2> make_pair (T1 x, T2 y) {
-		return ( pair<T1,T2>(x,y) );
-	}
+	// binary function
+	// 표준 이진 함수 객체의 기본 클래스
+	// 이항 함수 개체를 제공하는 파생 클래스에 상속될 수 있는 형식을 정의하는 빈 기본 구조체
+	template < class Arg1, class Arg2, class Result >
+	struct binary_function {
+		typedef Arg1 first_argument_type;
+		typedef Arg2 second_argument_type;
+		typedef Result result_type;
+	};
+
+	// operator< 와 동일한 반환값을 가진다.
+	template <class T>
+	struct less : binary_function<T, T, bool> {
+		bool operator()(const T& x, const T& y) const {
+			return (x < y);
+		}
+	};
 }	// namespace ft
 
 
