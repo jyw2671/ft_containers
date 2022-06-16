934300f1a6561557d5c047c709d658b29fcc4245
jyw2671 <yjung@student.42seoul.kr>
2022-06-16 19:31:41 +0900

    Typo correction

--- includes/utils.hpp
+++ includes/utils.hpp
@@ -143,7 +143,7 @@ namespace ft{
 		while (first1 != last1) {
 			if (first2 == last2 || comp(*first2, *first1))
 				return (false);
-			else if (cmop(*first1, *first2))
+			else if (comp(*first1, *first2))
 				return (true);
 			++first1;
 			++first2;
